f423dc8513ee09e5bf9a30172ff9b0a7f568be7c
Joe Legeckis <joseph.legeckis@dominionenterprises.com>
2014-01-28 10:10:27 -0500

    Cleaning up a bit

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var feeds = mongoose.model('Feeds', feedSchema);
 var history = mongoose.model('History', feedHistorySchema);
 
 function respond(req, res, next) {
-//	console.log(req.params);	
+	console.log(req.params);	
 /*	if (req.params.server === undefined) {
 	    return next(new restify.InvalidArgumentError('Server must be supplied'))
   	}*/
@@ -96,77 +96,6 @@ function dispatch(req, res, next) {
 	feedHistory.findOneAndUpdate({ feedid: req.params.feedid }, updateData, options, function (err) { if (err) { res.send(err); } });
 }
 
-/*function listEnvironments(req, res, next) { 
-	deployment.aggregate([{ $group: { _id: { environment: "$environment" } } } ], 
-		function(err, deploys) { res.send(deploys); });
-}
-
-function listServersPerEnvironment(req, res, next) {
-	deployment.aggregate([{ $group: { _id: { server: "$server"}, $find: { enviroment: req.params.name } } } ], 
-		function(err, deploys) { res.send(deploys); });
-}
-
-function listLatestPerEnvironment(req, res, next) {
-//	console.log("Quering..."+req.params.name);
-
-	deployment.aggregate([{$match:{"environment":req.params.name}},{$sort:{"datestamp":-1}},{$group:{_id:{server:"$server",codebase:"$codebase"},release:{$first:"$release"},datestamp:{$first:"$datestamp"},location:{$first:"$location"},server:{$first:"$server"},codebase:{$first:"$codebase"}}}], 
-//	deployment.find({ environment: req.params.name }, null, { sort: { datestamp: -1, codebase: 1, location: 1, server: -1 }},
-//	deployment.aggregate({ environment: req.params.name}).group({ _id : "$server"}).exec(
-		function (err, deploys) {
-			deploys.forEach(function(deploy) {
-				var outgoingDeployment = {
-					release: deploy._id.release,
-					datestamp: deploy.datestamp,
-//					md5: req.params.md5,
-					location: deploy.location,
-  					codebase: deploy._id.codebase,
-					server: deploys.server,
-//					success: 1,
-					environment: deploys.environment
-				};
-//				res.send(deploy);
-			});
-			res.send(deploys);
-	});
-}
-
-function listHistoryPerEnvironment(req, res, next) {
-	deployment.find({ environment: req.params.name }, null, { sort: { datestamp: -1, codebase: 1, location: 1, server: -1 }},function(err, deploys) { res.send(deploys); });
-//	deployment.aggregate([{ $group: { _id: { environment: req.params.name },  mostRecent: { $max: "$datestamp" } } } ], 
-//		function(err, deploys) { res.send(deploys); });
-}
-
-
-
-function listLatestForAll(req, res, next) {
-// This will group by server and release. It will have multiple server entries because each server will have multiple releases.
-	deployment.aggregate([{$project:{server: 1, release: 1, datestamp: 1}}, { $group: { _id: { server: "$server" },  mostRecent: { $max: "$datestamp" } } } ], 
-		function(err, deploys) { res.send(deploys); });
-}
-
-function pushRelease(req, res, next) {
-	console.log(req.params.tostring());
-	var options = {upsert: true};
-
-	var pushDeployment = {
-		release: req.params.release,
-		datestamp: Date.now(),
-		location: req.params.location,
-  		codebase: req.params.codebase,
-		environment: req.params.enviro
-	};
-
-	// Saving it to the database.
-	pushes.findOneAndUpdate({ release: req.params.release, codebase: req.params.codebase, location: req.params.location, environment: req.params.enviro }, pushDeployment, options, function (err) {
-		if (err) {console.log ('Error on save for '+req.params+' Error: '+err)} else { res.send('OK'); }
-	});
-}
-
-function getCurrent(req, res, next) {
-
-	pushes.find({ environment: req.params.enviro, location: req.params.location, codebase: req.params.codebase }, null, { sort: { datestamp: -1, codebase: 1, location: 1 }},function(err, deploys) { res.send(deploys); });
-}
-*/
 
 var server = restify.createServer();
 server.use(restify.bodyParser());
@@ -175,8 +104,6 @@ server.get('/hello/:name',function(req, res, next) { res.send("Hey, "+req.params
 server.post('/feed/:feedid',respond);
 server.get('/feed/:feedid',feedInfo);
 server.get('/dispatch/:server',dispatch);
-
-
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
